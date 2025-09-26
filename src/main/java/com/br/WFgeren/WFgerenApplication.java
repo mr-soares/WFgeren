f369db9919966c67f2a6d5303264e8e0ed015bc3
mr-soare <prrs321paulo@gmail.com>
2025-09-25 21:18:15 -0300

    base para o projeto usando o spring initializer

---
+++ src/main/java/com/br/WFgeren/WFgerenApplication.java
@@ -0,0 +1,13 @@
+package com.br.WFgeren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WFgerenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WFgerenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/WFgeren/WFgerenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.WFgeren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WFgerenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
